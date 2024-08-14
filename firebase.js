4f4daed2ccec540e694de726f970fec5a7887f36
Stanley Ke <kestanley101@gmail.com>
2024-08-14 13:02:18 -0400

    update

--- firebase.js
+++ firebase.js
@@ -1,13 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCXZVW2UTM232OXWWltXWURBvegYKls8l8",
+  authDomain: "pantry-tracker-a6a2c.firebaseapp.com",
+  projectId: "pantry-tracker-a6a2c",
+  storageBucket: "pantry-tracker-a6a2c.appspot.com",
+  messagingSenderId: "585775369584",
+  appId: "1:585775369584:web:685bf7792a0d15b49713b5",
+  measurementId: "G-338VBZ8QMV",
 };
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-export { firestore };
